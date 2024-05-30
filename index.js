ff274fb788222da11531ad3aab53dd5f66ba7189
hritikcps <hritik.raj@cloudprism.in>
2024-05-30 11:41:39 +0530

    tally_integration setup

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
